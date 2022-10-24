a380d6e858ebe808c7b550015421e082d0ddfc05
Turan <t.chelebi@viseven.com>
2022-10-24 22:27:39 +0300

    feat(#1): add plugin for check commit task number

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
+    rules: {
+        'check-task-number-rule': [2, 'always'],
+    },
+    plugins: [
+        {
+            rules: {
+                'check-task-number-rule': (data) => {
+                    const taskNumber = data.header.match(/(feat|fix|refactor)(.#(\d+)).:/gm);
+
+                    const correctCommit = data.header.includes(taskNumber) || false
+
+                    return [
+                        correctCommit,
+                        'your task number incorrect (feat|fix|refactor(#1))',
+                    ];
+                }
+            }
+        }
+    ]
 };
