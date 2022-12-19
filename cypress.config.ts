497b9d15c075fe5bcc65e0a22ac83f51ca1410db
Turan <t.chelebi@viseven.com>
2022-12-19 23:55:31 +0300

    feat(#1): add configs for ide and fix conflict between jest and cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,13 +2,13 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    supportFile: './src/test/e2e/support/e2e.{ts, tsx}',
-    specPattern: './src/test/e2e/spec/*.spec.{ts, tsx}',
+    supportFile: './cypress/support/e2e.{ts, tsx}',
+    specPattern: './cypress/spec/*.spec.{ts, tsx}',
   },
-  downloadsFolder: './src/test/e2e/downloads',
-  fileServerFolder: './src/test/e2e/fileServer',
-  fixturesFolder: './src/test/e2e/fixtures',
-  screenshotsFolder: './src/test/e2e/screenshots',
-  videosFolder: './src/test/e2e/videos',
-  supportFolder: './src/test/e2e/support',
+  downloadsFolder: './cypress/downloads',
+  fileServerFolder: './cypress/fileServer',
+  fixturesFolder: './cypress/fixtures',
+  screenshotsFolder: './cypress/screenshots',
+  videosFolder: './cypress/videos',
+  supportFolder: './cypress/support',
 });
---
+++ pages/_app.jsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types
+function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
