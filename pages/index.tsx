f5306ea53651c6d8c8a617115567fd3a0329b6b1
Yaroslav Kravtsov <Kravalg@users.noreply.github.com>
2023-12-11 23:00:25 +0100

    [Snyk] Upgrade next from 13.5.1 to 13.5.6 (#19)
    * fix: fix prettier command
    
    * fix: upgrade next from 13.5.1 to 13.5.6
    
    Snyk has created this PR to upgrade next from 13.5.1 to 13.5.6.
    
    See this package in npm:
    https://www.npmjs.com/package/next
    
    See this project in Snyk:
    https://app.snyk.io/org/kravalg/project/c9f6fff8-0f04-4112-9457-fa526d25dbc3?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * build(deps): fix pnpm-lock.yaml
    
    * build(deps): update lighthouse min requirements
    
    * fix: fix content visible on the screen
    
    * build(deps): update lhci/cli
    
    * fix: fix lighthouse performance check
    
    * fix: fix memory leak check
    
    * fix: fix eslint error
    
    * fix: lighthouserc.desktop.js
    
    * fix: lighthouserc.mobile.js
    
    ---------
    
    Co-authored-by: dmytrodoroshko <dmitry.doroshko.99@gmail.com>
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,21 +3,23 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 export default function Home() {
-  const { t} = useTranslation();
+  const { t } = useTranslation();
 
-  const click = () => {
+  const onClick = () => {
     setTimeout(() => {
+      // eslint-disable-next-line no-console
+      console.log('clicked');
     }, 2000);
   };
 
   return (
       <div>
         <Head>
-          <html lang="en"/>
           <title>Frontend SSR template</title>
           <meta name="description" content="Frontend SSR template is used for bootstrapping a project."/>
         </Head>
-        <button type='button' onClick={click}>{t('hello')}</button>
+        <button type='button' onClick={onClick}>{t('click')}</button>
+        <h1>Frontend SSR template</h1>
       </div>
   );
 }
