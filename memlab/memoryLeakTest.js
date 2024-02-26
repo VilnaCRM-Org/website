6eb1a96998f89999966d0881997ec262aae845a3
Vlas Dvorovyi <vlas20421@gmail.com>
2024-02-26 21:48:41 +0200

    fix(#4): fix memory-leak test

--- memlab/memoryLeakTest.js
+++ memlab/memoryLeakTest.js
@@ -3,5 +3,12 @@ require('dotenv').config();
 function url() {
   return process.env.WEBSITE_URL;
 }
+async function action() {
 
-module.exports = { url };
+}
+
+async function back() {
+
+}
+
+module.exports = { action, back, url };
