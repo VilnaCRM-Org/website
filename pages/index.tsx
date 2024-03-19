390e46b5893e5360b2fba13986a2a50c55b4ac08
Vlas Dvorovyi <vlas20421@gmail.com>
2024-03-19 16:08:26 +0200

    fix(#20): fix Ci checks

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  const { t } = useTranslation();
+
+  const click = () => {
+    setTimeout(() => {
+      // eslint-disable-next-line no-console
+      console.log('done');
+    }, 2000);
+  };
+
+  return (
+    <h1>
+      {/* eslint-disable-next-line react/button-has-type */}
+      <button onClick={click}>{t('coming-soon')}</button>
+    </h1>
+  );
 }
