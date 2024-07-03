5861c11dab8b30f6be5a57403e4ff7b088e8f19b
igor-pereimyvovk <igor.pereimyvovk@gmail.com>
2024-07-04 01:33:43 +0300

    refactor(#5): based on comments

--- src/features/landing/components/AuthSection/AuthForm/types.ts
+++ src/features/landing/components/AuthSection/AuthForm/types.ts
@@ -4,28 +4,3 @@ export interface AuthFormProps {
   onSubmit: (data: RegisterItem) => void;
   error?: string;
 }
-interface SignUpVariables {
-  input: {
-    email: string;
-    initials: string;
-    clientMutationId: string;
-    password: string;
-  };
-}
-
-interface SignUpResult {
-  data: {
-    signUp: {
-      success: boolean;
-    };
-  };
-  status: number;
-}
-
-export interface Mock {
-  request: {
-    variables: SignUpVariables;
-  };
-  result?: Promise<SignUpResult>;
-  error?: Error;
-}
