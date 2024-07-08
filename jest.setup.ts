4cd07f406fd1bfd60ab65ab3cf19574dfa7a38af
igor-pereimyvovk <igor.pereimyvovk@gmail.com>
2024-07-08 23:21:47 +0300

    test(#5): fix unit tests

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,2 +1,15 @@
 import '@testing-library/jest-dom';
-import './i18n';
+
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+
+import resources from './pages/i18n/localization.json';
+
+i18n.use(initReactI18next).init({
+  lng: 'en',
+  resources,
+  fallbackLng: process.env.NEXT_PUBLIC_FALLBACK_LANGUAGE,
+  interpolation: {
+    escapeValue: false,
+  },
+});
