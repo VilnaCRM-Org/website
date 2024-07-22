f74f3d6685a952e3e1f770cf977f0d361583314c
igor-pereimyvovk <igor.pereimyvovk@gmail.com>
2024-07-22 16:46:47 +0300

    refactor(#5): put delay in const variable

--- src/test/visual/visualComparison.spec.ts
+++ src/test/visual/visualComparison.spec.ts
@@ -3,6 +3,7 @@ import { test, expect } from '@playwright/test';
 import { screenSizes } from './constants';
 
 const currentLanguage: string = process.env.NEXT_PUBLIC_MAIN_LANGUAGE as string;
+const timeoutDuration: number = 3500;
 
 test.describe('Visual Tests', () => {
   for (const screen of screenSizes) {
@@ -12,16 +13,16 @@ test.describe('Visual Tests', () => {
       await page.waitForLoadState('networkidle');
       await page.evaluateHandle('document.fonts.ready');
 
-      await page.waitForTimeout(3500);
+      await page.waitForTimeout(timeoutDuration);
 
       const scrollHeight: number = await page.evaluate(() => document.documentElement.scrollHeight);
       await page.setViewportSize({ width: screen.width, height: scrollHeight });
 
-      await page.waitForTimeout(3500);
+      await page.waitForTimeout(timeoutDuration);
 
       await page.setViewportSize({ width: screen.width, height: screen.height });
 
-      await page.waitForTimeout(3500);
+      await page.waitForTimeout(timeoutDuration);
 
       await expect(page).toHaveScreenshot(`${screen.name}-${currentLanguage}.png`, {
         fullPage: true,
