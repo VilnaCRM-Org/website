660f8918bc5d2a7ea087f28600fce6223d45da75
igor-pereimyvovk <igor.pereimyvovk@gmail.com>
2024-09-12 13:37:10 +0300

    test(#5): add test to check for localization existence

--- src/config/i18nConfig.js
+++ src/config/i18nConfig.js
@@ -1,8 +1,22 @@
-const resources = require('../../pages/i18n/localization.json');
+const mainLanguage = process.env.NEXT_PUBLIC_MAIN_LANGUAGE;
+const fallbackLanguage = process.env.NEXT_PUBLIC_FALLBACK_LANGUAGE;
+
+if (!mainLanguage || !fallbackLanguage) {
+  throw new Error('Missing required environment variables for localization');
+}
+
+const getResources = () => {
+  try {
+    // eslint-disable-next-line global-require
+    return require('../../pages/i18n/localization.json');
+  } catch (error) {
+    throw new Error(`Failed to load localization resources: ${error.message}`);
+  }
+};
 
 module.exports = {
   lng: process.env.NEXT_PUBLIC_MAIN_LANGUAGE,
-  resources,
+  resources: getResources(),
   fallbackLng: process.env.NEXT_PUBLIC_FALLBACK_LANGUAGE,
   interpolation: {
     escapeValue: false,
