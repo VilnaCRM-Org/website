06cbf102e4faa2f077306efd54e04c99de73952b
igor-pereimyvovk <igor.pereimyvovk@gmail.com>
2024-09-27 00:43:07 +0300

    fix(#5): remove loading and error states from page

--- src/features/swagger/components/ApiDocumentation/ApiDocumentation.tsx
+++ src/features/swagger/components/ApiDocumentation/ApiDocumentation.tsx
@@ -5,15 +5,7 @@ import { useSwagger } from '../../hooks/useSwagger';
 const specUr: string = process.env.NEXT_PUBLIC_USER_SERVICE_OPENAI_SPEC_URL || '';
 
 function ApiDocumentation(): React.ReactElement {
-  const { yamlContent, loading, error } = useSwagger(specUr);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
+  const { yamlContent } = useSwagger(specUr);
 
   // return <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />;
   return <SwaggerUI spec={yamlContent as string} />;
--- src/features/swagger/hooks/useSwagger.ts
+++ src/features/swagger/hooks/useSwagger.ts
@@ -4,32 +4,19 @@ import { fetchSwaggerYaml } from '../api/fetchSwaggerYaml';
 
 type UseSwaggerReturn = {
   yamlContent: string | null;
-  loading: boolean;
-  error: string | null;
 };
 
 export const useSwagger: (url: string) => UseSwaggerReturn = url => {
   const [yamlContent, setYamlContent] = useState<string | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
     const fetchData: () => Promise<void> = async () => {
-      try {
-        setLoading(true);
-        const yaml: string = await fetchSwaggerYaml(url);
-        setYamlContent(yaml);
-      } catch (exception) {
-        if (exception instanceof Error) {
-          setError(exception.message);
-        }
-      } finally {
-        setLoading(false);
-      }
+      const yaml: string = await fetchSwaggerYaml(url);
+      setYamlContent(yaml);
     };
 
     fetchData();
   }, [url]);
 
-  return { yamlContent, loading, error };
+  return { yamlContent };
 };
