54d4cf8f4e29f14b7be00956cab54d6ca0becc24
igor-pereimyvovk <igor.pereimyvovk@gmail.com>
2024-10-12 20:11:10 +0300

    fix(#27): fix coderabbit comments

--- src/features/swagger/components/ApiDocumentation/ApiDocumentation.tsx
+++ src/features/swagger/components/ApiDocumentation/ApiDocumentation.tsx
@@ -7,7 +7,6 @@ const specUrl: string = process.env.NEXT_PUBLIC_USER_SERVICE_OPENAI_SPEC_URL ?? 
 function ApiDocumentation(): React.ReactElement {
   const { yamlContent } = useSwagger(specUrl);
 
-  // return <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />;
   return <SwaggerUI spec={yamlContent as string} />;
 }
 
