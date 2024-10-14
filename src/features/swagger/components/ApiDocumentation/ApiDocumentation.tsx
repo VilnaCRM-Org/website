b4677a95817297af45425e81b628ccde7af82eb4
igor-pereimyvovk <igor.pereimyvovk@gmail.com>
2024-10-14 22:42:24 +0300

    fix(#27): fix coderabbit comments

--- src/features/swagger/components/ApiDocumentation/ApiDocumentation.tsx
+++ src/features/swagger/components/ApiDocumentation/ApiDocumentation.tsx
@@ -4,10 +4,10 @@ import { useSwagger } from '../../hooks/useSwagger';
 
 const specUrl: string = process.env.NEXT_PUBLIC_USER_SERVICE_OPENAI_SPEC_URL ?? '';
 
-function ApiDocumentation(): React.ReactElement {
+function ApiDocumentation(): React.ReactElement | null {
   const { yamlContent } = useSwagger(specUrl);
 
-  return <SwaggerUI spec={yamlContent as string} />;
+  return yamlContent ? <SwaggerUI spec={yamlContent} /> : null;
 }
 
 export default ApiDocumentation;
