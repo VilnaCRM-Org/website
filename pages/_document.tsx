aedda92c581e608c03705187a57a1f5911baa813
igor-pereimyvovk <igor.pereimyvovk@gmail.com>
2024-11-21 01:14:05 +0200

    fix(#40): fix bugs based on swagger bugs document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,9 @@ const mainLanguage: string = process.env.NEXT_PUBLIC_MAIN_LANGUAGE as string;
 export default function Document(): React.ReactElement {
   return (
     <Html lang={mainLanguage}>
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
