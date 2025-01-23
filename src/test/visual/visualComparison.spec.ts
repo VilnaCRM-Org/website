293d0c5b51d84170da96f6728c97a988cbf958e0
Vladomira <juliavladomira@gmail.com>
2025-01-23 21:38:46 +0100

    chore: update e2e workflow to use actions/upload-artifact@v4

--- src/test/visual/visualComparison.spec.ts
+++ src/test/visual/visualComparison.spec.ts
@@ -26,6 +26,7 @@ test.describe('Visual Tests', () => {
 
       await expect(page).toHaveScreenshot(`${currentLanguage}_${screen.name}.png`, {
         fullPage: true,
+        maxDiffPixelRatio: 0.01,
       });
     });
   }
