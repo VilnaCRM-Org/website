f8c1a0c012b0e782448dda8b63344a28b91d3fff
DianaDuk <dianadukelska@gmail.com>
2025-02-01 17:55:50 +0000

    feat(ui): redesign component by replacing old structure

--- src/features/landing/components/ForWhoSection/ForWhoSection.tsx
+++ src/features/landing/components/ForWhoSection/ForWhoSection.tsx
@@ -1,26 +1,21 @@
 import { Box, Container } from '@mui/material';
-import { ImageProps } from 'next/image';
-import { getOptimizedImageProps } from 'next-export-optimize-images/image';
 import React from 'react';
-
-import DesktopVectorIcon from '../../assets/img/about-vilna/FrameDesktop.png';
-import TabletVectorIcon from '../../assets/img/about-vilna/FrameTablet.png';
 
 import { Cards } from './Cards';
 import MainTitle from './MainTitle/MainTitle';
 import styles from './styles';
+import bigScreen from '../../assets/img/about-vilna/mobileScreen.png';
+import smallScreen from '../../assets/img/about-vilna/mobile2Screen.png';
+import waves from '../../assets/svg/for-who/waves.svg';
+import point6 from '../../assets/svg/for-who/point6.svg';
+import point8 from '../../assets/svg/for-who/point8.svg';
+import point10 from '../../assets/svg/for-who/point10.svg';
+import circle from '../../assets/svg/for-who/circle.svg';
+import romb from '../../assets/svg/for-who/romb.svg';
+import hexagon from '../../assets/svg/for-who/hexagon.svg';
+import triangle from '../../assets/svg/for-who/triangle.svg';
 
 function ForWhoSection(): React.ReactElement {
-  const iconAltText: string = 'Vector';
-
-  const tabletProps: ImageProps = getOptimizedImageProps({
-    src: TabletVectorIcon,
-    alt: iconAltText,
-  }).props;
-  const desktopProps: ImageProps = getOptimizedImageProps({
-    src: DesktopVectorIcon,
-    alt: iconAltText,
-  }).props;
 
   return (
     <Box id="forWhoSection" component="section" sx={styles.wrapper}>
@@ -30,24 +25,24 @@ function ForWhoSection(): React.ReactElement {
           <Box sx={styles.lgCardsWrapper}>
             <Cards />
           </Box>
-
-          <Box sx={styles.mainImage}>
-            <picture>
-              <source
-                srcSet={tabletProps.src as string}
-                width={tabletProps.width}
-                height={tabletProps.height}
-                media="(max-width: 1130.98px)"
-              />
-              <img
-                src={desktopProps.src as string}
-                width={desktopProps.width}
-                height={desktopProps.height}
-                alt={desktopProps.alt}
-                loading="lazy"
-              />
-            </picture>
-          </Box>
+          <div className='container'>
+            <div className='svg-container'>
+            <img src={circle} alt="circle" className="circle" />
+            <img src={romb} alt="romb" className="romb" />
+              <div className="point-container">
+            <img src={point6} alt="point6" className="point6" />
+            <img src={point8} alt="point8" className="point8" />
+            <img src={point10} alt="point10" className="point10" />
+              </div>
+            </div>
+           <div className='square'>
+            <img src={bigScreen} alt="bigScreen" className="bigScreen" />
+            <img src={smallScreen} alt="smallScreen" className="smallScreen" />
+            <img src={waves} alt="waves" className="waves" />
+            <img src={hexagon} alt="hexagon" className="hexagon" />
+           </div>
+           <img src={triangle} alt="triangle" className="triangle" />
+          </div>
         </Box>
       </Container>
       <Box sx={styles.smCardsWrapper}>
