4fc66f081ba391f1ccf5b0851a328fa49670fef7
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-02-26 00:19:57 +0200

    refactor(#133): improve script structure

--- scripts/cloudfront_routing.js
+++ scripts/cloudfront_routing.js
@@ -1,12 +1,20 @@
+/**
+ * CloudFront Function to handle URL routing for static assets
+ * - Routes /index.html to /
+ * - Routes /swagger.html to /swagger
+ * 
+ * @param {Object} event - The CloudFront function event object
+ * @returns {Object} The modified event object
+ */
 function handler(event) {
-    var request = event.request;
-    var uri = request.uri;
+    const request = event.request;
+    const uri = request.uri;
 
     if (uri === "/index.html") {
         request.uri = "/";
     } else if (uri === "/swagger.html") {
         request.uri = "/swagger";
     }
-
-    return request;
+    
+    return event;
 }
