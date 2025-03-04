25dff65ffa4b32ae9dda67955706d6154cb65bca
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-03-04 18:28:26 +0200

    refactor(#133): using ES5.1-compatible syntax

--- scripts/cloudfront_routing.js
+++ scripts/cloudfront_routing.js
@@ -2,23 +2,24 @@
  * CloudFront Function to handle URL routing for static assets
  * - Routes /index.html to /
  * - Routes /swagger.html to /swagger
+ * - Preserves query parameters
  * 
  * @param {Object} event - The CloudFront function event object
  * @returns {Object} The modified event object
  */
 function handler(event) {
-    const request = event.request;
-    const uri = request.uri;
+    var request = event.request;
+    var uri = request.uri;
 
     if (uri === "/index.html") {
         request.uri = "/";
     } else if (uri === "/swagger.html") {
         request.uri = "/swagger";
-    }
-    else if (uri.startsWith("/index.html?")) {
+    } else if (uri.indexOf("/index.html?") === 0) {
         request.uri = "/" + uri.substring("/index.html".length);
-    } else if (uri.startsWith("/swagger.html?")) {
+    } else if (uri.indexOf("/swagger.html?") === 0) {
         request.uri = "/swagger" + uri.substring("/swagger.html".length);
     }
+
     return event;
 }
