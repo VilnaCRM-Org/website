1d6465d2b399590c1ceab8f296d93d7a608a7b2b
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-03-06 18:48:23 +0200

    fix(#133): add debug steps

--- scripts/cloudfront_routing.js
+++ scripts/cloudfront_routing.js
@@ -1,31 +1,33 @@
 function handler(event) {
     var request = event.request;
     var uri = request.uri;
-  
-    // Route mapping for static files and specific routes
-    var routeMap = {
-      "/": "/index.html",               // Root path -> serve the index.html file
-      "/about": "/about/index.html",    // '/about' -> '/about/index.html'
-      "/about/": "/about/index.html",   // '/about/' -> '/about/index.html'
-      "/en": "/en/index.html",          // '/en' -> '/en/index.html' (English homepage)
-      "/en/": "/en/index.html",         // '/en/' -> '/en/index.html'
-      "/swagger": "/swagger.html",      // '/swagger' -> '/swagger.html'
-      "/test": "/_next/static/media/desktop.0ec56f83.jpg" // Specific file for '/test'
-    };
-  
-    // If the URI matches any key in the routeMap, rewrite it
-    if (routeMap[uri] !== undefined) {
-      uri = routeMap[uri];
-    } else {
-      // If the URI is a directory or does not have an extension, add 'index.html'
-      if (uri.charAt(uri.length - 1) === '/') {
-        uri += 'index.html';
-      } else if (uri.indexOf('.') === -1) {
-        uri += '/index.html';
-      }
+
+    try {
+        if (!uri || typeof uri !== "string") {
+            return request;
+        }
+
+        var routeMap = {
+            "/": "/index.html",
+            "/about": "/about/index.html",
+            "/about/": "/about/index.html",
+            "/en": "/en/index.html",
+            "/en/": "/en/index.html",
+            "/swagger": "/swagger.html",
+            "/test": "/_next/static/media/desktop.0ec56f83.jpg"
+        };
+
+        if (routeMap[uri] !== undefined) {
+            uri = routeMap[uri];
+        } else if (uri.substr(-1) === '/') {
+            uri += 'index.html';
+        } else if (uri.indexOf('.') === -1) {
+            uri += '/index.html';
+        }
+
+        request.uri = uri;
+        return request;
+    } catch (e) {
+        return request; // Возвращаем request без изменений в случае ошибки
     }
-  
-    // Update the request URI and return the modified request object
-    request.uri = uri;
-    return request;
-  }
+}
