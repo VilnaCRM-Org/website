038dabb892e9c89bb81e59a17762da1abc9cc631
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-03-06 17:04:15 +0200

    fix(#133): update script according ES5.1

--- scripts/cloudfront_routing.js
+++ scripts/cloudfront_routing.js
@@ -1,38 +1,31 @@
 function handler(event) {
-    const request = event.request;
-    let uri = request.uri;
-    
-    // Mapping of routes to their rewritten URIs for dynamic routing
-    const routeMap = {
+    var request = event.request;
+    var uri = request.uri;
+  
+    // Route mapping for static files and specific routes
+    var routeMap = {
       "/": "/index.html",               // Root path -> serve the index.html file
       "/about": "/about/index.html",    // '/about' -> '/about/index.html'
       "/about/": "/about/index.html",   // '/about/' -> '/about/index.html'
-      // Add other routes that need special handling:
-      // e.g., old paths redirected to new paths or default documents for sections.
-      // Example:
-      // "/old-page": "/new-page",      // redirect '/old-page' -> '/new-page'
-      // Internationalization (i18n) examples for language-specific homepages:
       "/en": "/en/index.html",          // '/en' -> '/en/index.html' (English homepage)
       "/en/": "/en/index.html",         // '/en/' -> '/en/index.html'
-      // You can add "/fr": "/fr/index.html", "/fr/": "/fr/index.html", etc., for other languages.
-      "/swagger": "/swagger.html",
-      "/test": "/_next/static/media/desktop.0ec56f83.jpg"
+      "/swagger": "/swagger.html",      // '/swagger' -> '/swagger.html'
+      "/test": "/_next/static/media/desktop.0ec56f83.jpg" // Specific file for '/test'
     };
   
-    // If the request URI matches a key in routeMap, rewrite it
-    if (routeMap.hasOwnProperty(uri)) {
+    // If the URI matches any key in the routeMap, rewrite it
+    if (routeMap[uri] !== undefined) {
       uri = routeMap[uri];
     } else {
-      // If URI is a directory (ends with '/') or a clean path with no file extension, append 'index.html'
-      if (uri.endsWith('/')) {
+      // If the URI is a directory or does not have an extension, add 'index.html'
+      if (uri.charAt(uri.length - 1) === '/') {
         uri += 'index.html';
-      } else if (uri.lastIndexOf('.') === -1) {
-        // No '.' found in the URI path => likely a clean path (no file extension)
+      } else if (uri.indexOf('.') === -1) {
         uri += '/index.html';
       }
     }
   
-    // Update the request URI and return the request object
+    // Update the request URI and return the modified request object
     request.uri = uri;
     return request;
   }
