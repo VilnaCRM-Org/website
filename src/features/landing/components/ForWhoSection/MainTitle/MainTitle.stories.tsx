3e257927e5c99774acc20c7bf2704d55f37dd9af
DianaDuk <dianadukelska@gmail.com>
2025-04-21 19:33:09 +0000

    feat(#28): merge from old branch to new

---
+++ src/features/landing/components/ForWhoSection/MainTitle/MainTitle.stories.tsx
@@ -0,0 +1,18 @@
+import { Meta, StoryObj } from '@storybook/react';
+
+import MainTitle from './MainTitle';
+
+const meta: Meta<typeof MainTitle> = {
+  title: 'Components/ForWhoSection/MainTitle',  
+  component: MainTitle,
+  tags: ['autodocs'],
+  parameters: {
+    layout: '',  
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof MainTitle>;
+
+export const Default: Story = {};
