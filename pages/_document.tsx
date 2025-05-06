8b8baf462ffee688dc8c60da5100dac14c16db60
Vladomira <juliavladomira@gmail.com>
2025-05-06 17:27:20 +0200

    chore(#158): update favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,21 +10,25 @@ export default function Document(): React.ReactElement {
         <meta name="application-name" content="VilnaCRM" />
         <meta name="theme-color" content="#ffffff" />
 
-        <link rel="icon" type="image/svg+xml" sizes="16x16" href="/layout/favicon/16х16.svg" />
-        <link rel="icon" type="image/svg+xml" sizes="32x32" href="/layout/favicon/32х32.svg" />
-        <link rel="icon" type="image/svg+xml" sizes="64x64" href="/layout/favicon/64х64.svg" />
-        <link rel="icon" type="image/svg+xml" sizes="96x96" href="/layout/favicon/96х96.svg" />
-        <link rel="icon" type="image/svg+xml" sizes="128x128" href="/layout/favicon/128х128.svg" />
-        <link rel="icon" type="image/svg+xml" sizes="256x256" href="/layout/favicon/256х256.svg" />
-        <link rel="icon" type="image/svg+xml" sizes="512x512" href="/layout/favicon/512х512.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="16x16" href="/layout/favicon/16x16.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="32x32" href="/layout/favicon/32x32.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="64x64" href="/layout/favicon/64x64.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="96x96" href="/layout/favicon/96x96.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="128x128" href="/layout/favicon/128x128.svg" />
+        <link rel="icon" type="image/svg+xml" sizes="256x256" href="/layout/favicon/256x256.svg" />
+        <link rel="icon" type="image/svg+XML" sizes="512x512" href="/layout/favicon/512x512.svg" />
         <link
           rel="icon"
           type="image/svg+xml"
           sizes="1024x1024"
-          href="/layout/favicon/1024х1024.svg"
+          href="/layout/favicon/1024x1024.svg"
         />
 
-        <link rel="shortcut icon" href="/layout/favicon/32х32.svg" />
+        <link rel="shortcut icon" href="/layout/favicon/32x32.svg" />
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/layout/favicon/apple-icon.png" />
+
+        <link rel="manifest" href="/layout/favicon/site.webmanifest" />
       </Head>
       <body>
         <Main />
