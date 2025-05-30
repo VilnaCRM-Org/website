f3dcbf089366ae43ff1b9757f6dcbfbee3eb9873
Vladomira <juliavladomira@gmail.com>
2025-05-30 19:01:01 +0200

    chore(#29): update fetchSwaggerYaml

--- src/features/swagger/api/fetchSwaggerYaml.ts
+++ src/features/swagger/api/fetchSwaggerYaml.ts
@@ -1,8 +1,3 @@
-type GithubFileResponse = {
-  content: string;
-  encoding: string;
-};
-
 export const fetchSwaggerYaml: (url: string) => Promise<string> = async url => {
   const response: Response = await fetch(url);
 
@@ -10,7 +5,5 @@ export const fetchSwaggerYaml: (url: string) => Promise<string> = async url => {
     throw new Error(`Failed to fetch: ${response.statusText}`);
   }
 
-  const data: GithubFileResponse = await response.json();
-
-  return Buffer.from(data.content, 'base64').toString('utf-8');
+  return response.text();
 };
