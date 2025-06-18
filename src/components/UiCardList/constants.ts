a203850f63d6fc34b97e4ddc42ebb5871f8e419c
Илья Григорьев <lizarthej@gmail.com>
2025-06-19 00:53:34 +0300

    fix(UiCardItem): add missing image property to CardItem objects

--- src/components/UiCardList/constants.ts
+++ src/components/UiCardList/constants.ts
@@ -2,7 +2,9 @@ import Diamond      from '../../features/landing/assets/svg/possibilities/diamon
 import Ruby         from '../../features/landing/assets/svg/possibilities/ruby.svg';
 import SmallDiamond from '../../features/landing/assets/svg/possibilities/smallDiamond.svg';
 import SmallRuby    from '../../features/landing/assets/svg/possibilities/smallRuby.svg';
+
 import type { Card } from '../../features/landing/types/Card/card-item';
+
 import getWhyUsCards from './cardData';
 import { CardItem } from './types';
 
@@ -10,8 +12,8 @@ const whyUsCards: Card[] = getWhyUsCards();
 
 export const LARGE_CARDLIST_ARRAY: CardItem[] = whyUsCards.map(card => ({
   ...card,
+  image: card.image || '', 
   imageSrc: card.image || Diamond,
-  
 }));
 
 export const SMALL_CARDLIST_ARRAY: CardItem[] = [
@@ -19,6 +21,7 @@ export const SMALL_CARDLIST_ARRAY: CardItem[] = [
     type: 'smallCard',
     id: 'item_1',
     imageSrc: Ruby,
+    image: Ruby,
     text: 'unlimited_possibilities.cards_texts.text_for_cases',
     title: 'unlimited_possibilities.cards_headings.heading_public_api',
     alt: 'unlimited_possibilities.card_image_titles.title_for_first',
@@ -27,6 +30,7 @@ export const SMALL_CARDLIST_ARRAY: CardItem[] = [
     type: 'smallCard',
     id: 'item_2',
     imageSrc: SmallDiamond,
+    image: Ruby,
     text: 'unlimited_possibilities.cards_texts.text_integrate',
     title: 'unlimited_possibilities.cards_headings.heading_ready_plugins',
     alt: 'unlimited_possibilities.card_image_titles.title_for_second',
@@ -35,6 +39,7 @@ export const SMALL_CARDLIST_ARRAY: CardItem[] = [
     type: 'smallCard',
     id: 'item_3',
     imageSrc: SmallRuby,
+    image: Ruby,
     text: 'unlimited_possibilities.cards_texts.text_get_data',
     title: 'unlimited_possibilities.cards_headings.heading_system',
     alt: 'unlimited_possibilities.card_image_titles.title_for_third',
@@ -43,6 +48,7 @@ export const SMALL_CARDLIST_ARRAY: CardItem[] = [
     type: 'smallCard',
     id: 'item_4',
     imageSrc: Diamond,
+    image: Ruby,
     text: 'unlimited_possibilities.cards_texts.text_for_custom',
     title: 'unlimited_possibilities.cards_headings.heading_libraries',
     alt: 'unlimited_possibilities.card_image_titles.title_for_fourth',
