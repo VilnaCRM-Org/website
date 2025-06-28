b4b4e59fec5592b869e521124bf29db7baeff8b4
MEPO <lizarthej@gmail.com>
2025-06-29 01:25:50 +0300

    fix: prevent crash when src is undefined in CustomImage

--- src/features/landing/components/ForWhoSection/ForWhoSection.tsx
+++ src/features/landing/components/ForWhoSection/ForWhoSection.tsx
@@ -85,7 +85,7 @@ function ForWhoSection(): React.ReactElement {
             <Box sx={styles.square}>
             <Box sx={styles.bigScreen}>
               <Box sx={styles.bigScreen}>
-  <img src="/assets/img/about-vilna/desktop.jpg" alt={t('alts.big_screen')} loading="lazy" />
+         <img alt={t('alts.big_screen')} {...getImageProps('/assets/img/about-vilna/desktop.jpg')} loading="lazy" />
 </Box>
   <Box sx={styles.smallScreen}>
     <img src="/assets/img/about-vilna/mobile.jpg" alt={t('alts.small_screen')} loading="lazy" />
