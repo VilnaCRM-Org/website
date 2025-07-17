9864d10602e2242067744c680bc4a64b00a6a0be
MEPO <lizarthej@gmail.com>
2025-07-17 23:16:57 +0300

    test

--- src/features/landing/components/AuthSection/AuthForm/FormInput.tsx
+++ src/features/landing/components/AuthSection/AuthForm/FormInput.tsx
@@ -6,42 +6,56 @@ import { UiTextFieldForm, UiTypography } from '@/components';
 
 import { AuthFormProps } from './types';
 
-
 interface FormInputProps {
   inputTitle: React.CSSProperties;
   control: AuthFormProps['control'];
   id: string;
-name: 'FullName' | 'Email' | 'Password' | 'Privacy';
+  name: 'FullName' | 'Email' | 'Password' | 'Privacy';
   rules: RegisterOptions<FieldValues, string> | null;
   placeholder: string;
   type: string;
-    htmlFor: string;
-    component?: 'a' | 'div' | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'label' | 'p' | 'section' | 'span';
+  htmlFor: string;
+  component?:
+    | 'a'
+    | 'div'
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'h5'
+    | 'h6'
+    | 'label'
+    | 'p'
+    | 'section'
+    | 'span';
 }
 
-function FormInput({ inputTitle, control, id, name, rules, placeholder, type, htmlFor,component}: FormInputProps): JSX.Element {
+function FormInput({
+  inputTitle,
+  control,
+  id,
+  name,
+  rules,
+  placeholder,
+  type,
+  htmlFor,
+  component,
+}: FormInputProps): JSX.Element {
   const { t } = useTranslation();
   return (
-   <div>
-          <UiTypography
-            variant="medium14"
-            sx={inputTitle}
-            component={component}
-            htmlFor={htmlFor}
-          >
-            {t('sign_up.form.name_input.label')}
-          </UiTypography>
-          <UiTextFieldForm
-            id={id}
-            control={control}
-            name={name}
-            rules={rules ?? {}}
-
-            placeholder={placeholder}
-            type={type}
-          />
-
-  </div>
+    <div>
+      <UiTypography variant="medium14" sx={inputTitle} component={component} htmlFor={htmlFor}>
+        {t('sign_up.form.name_input.label')}
+      </UiTypography>
+      <UiTextFieldForm
+        id={id}
+        control={control}
+        name={name}
+        rules={rules ?? {}}
+        placeholder={placeholder}
+        type={type}
+      />
+    </div>
   );
 }
 FormInput.defaultProps = {
