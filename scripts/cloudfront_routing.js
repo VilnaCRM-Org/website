37f89b9fad5224291b98d42a0d556472eb38e8a4
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-09-04 18:14:06 +0300

    fix(#226): resolve error response

--- scripts/cloudfront_routing.js
+++ scripts/cloudfront_routing.js
@@ -4,39 +4,40 @@
  */
 'use strict';
 function handler(event) {
-  var request = event.request;
+    var request = event.request;
 
-  if (!request.uri || typeof request.uri !== 'string') {
-    return request;
-  }
-
-  try {
-    var uri = request.uri;
-
-    var routeMap = {
-      '/': '/index.html',
-      '/about': '/about/index.html',
-      '/about/': '/about/index.html',
-      '/en': '/en/index.html',
-      '/en/': '/en/index.html',
-      '/swagger': '/swagger.html',
-    };
-
-    if (routeMap[uri] !== undefined) {
-      request.uri = routeMap[uri];
-      return request;
+    if (!request.uri || typeof request.uri !== 'string') {
+        return request;
     }
 
-    if (uri.substr(-1) === '/') {
-      uri += 'index.html';
-    } else if (uri.indexOf('.') === -1) {
-      uri += '/index.html';
-    }
+    try {
+        var uri = request.uri;
 
-    request.uri = uri;
-    return request;
-  } catch (error) {
-    console.log('CloudFront Function error:', error);
-    return request;
-  }
+        var routeMap = {
+            '/': '/index.html',
+            '/about': '/about/index.html',
+            '/about/': '/about/index.html',
+            '/en': '/en/index.html',
+            '/en/': '/en/index.html',
+            '/swagger': '/swagger.html',
+        };
+
+        if (routeMap[uri] !== undefined) {
+            request.uri = routeMap[uri];
+            return request;
+        }
+
+        if (uri.endsWith('/')) {
+            request.uri = uri + 'index.html';
+        }
+
+        else if (uri.indexOf('.') === -1 && uri.split('/').length > 2) {
+            request.uri = uri + '/index.html';
+        }
+
+        return request;
+    } catch (error) {
+        console.log('CloudFront Function error:', error);
+        return request;
+    }
 }
