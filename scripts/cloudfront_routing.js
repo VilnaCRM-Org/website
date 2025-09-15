7f89a0314e561afc4a11d3aa0f0c2510df993bc4
Mykyta Mozhgovoi <n.mozgowoi2017@gmail.com>
2025-09-15 18:41:53 +0300

    revert(cloudfront): remove /.well-known/ rewrite exemption

--- scripts/cloudfront_routing.js
+++ scripts/cloudfront_routing.js
@@ -4,49 +4,49 @@
  */
 'use strict';
 var ROUTE_MAP = Object.freeze({
-    '/': '/index.html',
-    '/about': '/about/index.html',
-    '/about/': '/about/index.html',
-    '/en': '/en/index.html',
-    '/en/': '/en/index.html',
-    '/swagger': '/swagger.html',
+  '/': '/index.html',
+  '/about': '/about/index.html',
+  '/about/': '/about/index.html',
+  '/en': '/en/index.html',
+  '/en/': '/en/index.html',
+  '/swagger': '/swagger.html',
 });
 function handler(event) {
-    var request = event.request;
+  var request = event.request;
 
-    if (!request.uri || typeof request.uri !== 'string') {
-        var host = (request.headers && request.headers.host && request.headers.host.value) || '';
-        console.log('cloudfront_routing: missing/invalid request.uri', 'host=', host, 'uri=', request.uri);
-        return request;
+  if (!request.uri || typeof request.uri !== 'string') {
+    var host = (request.headers && request.headers.host && request.headers.host.value) || '';
+    console.log(
+      'cloudfront_routing: missing/invalid request.uri',
+      'host=',
+      host,
+      'uri=',
+      request.uri
+    );
+    return request;
+  }
+
+  try {
+    var uri = request.uri;
+    var segments = uri.split('/');
+    var lastSegment = segments[segments.length - 1];
+
+    if (Object.prototype.hasOwnProperty.call(ROUTE_MAP, uri)) {
+      request.uri = ROUTE_MAP[uri];
+      return request;
     }
 
-    try {
-        var uri = request.uri;
-        var segments = uri.split('/');
-        var lastSegment = segments[segments.length - 1];
-
-        if (Object.prototype.hasOwnProperty.call(ROUTE_MAP, uri)) {
-            request.uri = ROUTE_MAP[uri];
-            return request;
-        }
-
-        var lastChar = (typeof uri === 'string') ? uri.charAt(uri.length - 1) : '';
-        if (lastChar === '/') {
-            request.uri = uri + 'index.html';
-            return request;
-        }
-
-        else if (lastSegment.indexOf('.') === -1 && segments.length > 2) {
-            // Bypass rewrites for well-known challenges or other explicit files without extensions
-            if (uri.indexOf('/.well-known/') === 0) {
-                return request;
-            }
-            request.uri = uri + '/index.html';
-        }
-
-        return request;
-    } catch (error) {
-        console.log('cloudfront_routing: error', error);
-        return request;
+    var lastChar = typeof uri === 'string' ? uri.charAt(uri.length - 1) : '';
+    if (lastChar === '/') {
+      request.uri = uri + 'index.html';
+      return request;
+    } else if (lastSegment.indexOf('.') === -1 && segments.length > 2) {
+      request.uri = uri + '/index.html';
     }
+
+    return request;
+  } catch (error) {
+    console.log('cloudfront_routing: error', error);
+    return request;
+  }
 }
